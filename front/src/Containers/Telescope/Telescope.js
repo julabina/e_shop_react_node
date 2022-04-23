edf0b37f1e8878fe2982706f8299f30a8be675ca
Lenfumé Julien <julabina@hotmail.fr>
2022-04-23 11:29:49 +0200

    add base telescope list

--- front/src/Containers/Telescope/Telescope.js
+++ front/src/Containers/Telescope/Telescope.js
@@ -2,9 +2,27 @@ import React from 'react';
 
 const Telescope = () => {
     return (
-        <div>
-            
-        </div>
+        <main>
+            <section className='telescopesList'>
+                <div className="telescopesList__top">
+                    <div className="telescopesList__top__top">
+                        <h2>Télescopes et Lunettes astronomiques</h2>
+                        <div className="telescopesList__top__top__pages"></div>
+                    </div>
+                    <select name="" id="">
+                        <option value=""></option>
+                        <option value=""></option>
+                        <option value=""></option>
+                    </select>
+                </div>
+                <div className="telescopesList__main">
+
+                </div>
+                <div className="telescopesList__bot">
+                    
+                </div>
+            </section>
+        </main>
     );
 };
 
