b7285b742b85275ce170b1abbfb10fb5e756b0d0
Lenfumé Julien <julabina@hotmail.fr>
2022-05-05 10:09:47 +0200

    make contact form + css

--- front/src/Containers/Contact/Contact.js
+++ front/src/Containers/Contact/Contact.js
@@ -1,10 +1,45 @@
 import React from 'react';
 
 const Contact = () => {
+
+
+    
     return (
-        <div>
-            
-        </div>
+        <main>
+            <section className='contact'>
+                <h2>Contactez nous</h2>
+                <div className="contact__separator"></div>
+                <form action="" className='contact__form'>
+                    <div className="contact__form__row">
+                        <div className="contact__form__row__inputCont">
+                            <label htmlFor="">Prénom</label>
+                            <input type="text" name="" id="" />
+                        </div>
+                        <div className="contact__form__row__inputCont">                            
+                            <label htmlFor="">Nom</label>
+                            <input type="text" name="" id="" />
+                        </div>
+                    </div>
+                    <div className="contact__form__row">
+                        <div className="contact__form__row__inputCont">
+                            <label htmlFor="">Email</label>
+                            <input type="mail" name="" id="" />
+                        </div>
+                        <div className="contact__form__row__inputCont">                            
+                            <label htmlFor="">Tel</label>
+                            <input type="number" name="" id="" />
+                        </div>
+                    </div>
+                    <div className="contact__form__message">
+                        <label htmlFor="">Message</label>
+                        <textarea name="" id=""></textarea>
+                    </div>
+                    <div className="contact__form__btnCont">
+                        <button>Envoyer</button>
+                    </div>
+                </form>
+            </section>
+        </main>
     );
 };
 
