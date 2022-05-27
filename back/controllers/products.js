da670b33d9999b536ebbf3ab23a036a33f7e05d8
Lenfumé Julien <julabina@hotmail.fr>
2022-05-27 14:58:47 +0200

    make ctllr and route for products

--- back/controllers/products.js
+++ back/controllers/products.js
@@ -1,28 +1,257 @@
-const { Product, ProductAttribute, Brand } = require('../db/sequelize');
+const { Product, ProductAttribute, Brand, Category, TelescopeType, OculaireCollection, MountType } = require('../db/sequelize');
 
 exports.findAllTelescope = (req, res, next) => {
     
     Product.hasMany(ProductAttribute, {foreignKey: 'productId'});
     ProductAttribute.belongsTo(Product);
+    Category.hasMany(Product, {foreignKey: 'categoryId'})
+    Product.belongsTo(Category);
     Brand.hasMany(ProductAttribute, {foreignKey: 'brandId'})
     ProductAttribute.belongsTo(Brand);
+    TelescopeType.hasMany(ProductAttribute, {foreignKey: 'telescopeTypeId'})
+    ProductAttribute.belongsTo(TelescopeType);
     
 
     Product.findAll({
         include : [
-        {
-            model: ProductAttribute,
-            include: {
-                model: Brand
-            }
-        },  
-    ]
-
+            {
+                model: ProductAttribute,
+                include: {
+                    model: Brand,
+                }
+            }, 
+            {
+                model: ProductAttribute,
+                include: {
+                    model: TelescopeType,
+                }
+            }, 
+            {
+                model: Category,
+                where : {
+                    name: "telescope"
+                }
+            } 
+        ]        
     })
     .then(data => {
-        res.status(200).json({data})
+        const message = "La liste des telescopes a bien été récupéré."
+        res.status(200).json({message, data})
     })
     .catch(error => {
         res.status(500).json({error})
     })
-}
+};
+
+exports.findAllOculaire = (req, res, next) => {
+       
+    Product.hasMany(ProductAttribute, {foreignKey: 'productId'});
+    ProductAttribute.belongsTo(Product);
+    Category.hasMany(Product, {foreignKey: 'categoryId'})
+    Product.belongsTo(Category);
+    Brand.hasMany(ProductAttribute, {foreignKey: 'brandId'})
+    ProductAttribute.belongsTo(Brand);
+    OculaireCollection.hasMany(ProductAttribute, {foreignKey: 'oculaireModelId'})
+    ProductAttribute.belongsTo(OculaireCollection);
+    
+
+    Product.findAll({
+        include : [
+            {
+                model: ProductAttribute,
+                include: {
+                    model: Brand
+                }
+            }, 
+            {
+                model: ProductAttribute,
+                include: {
+                    model: OculaireCollection
+                }
+            }, 
+            {
+                model: Category,
+                where : {
+                    name: "oculaire"
+                }
+            } 
+        ]        
+    })
+    .then(data => {
+        const message = "La liste des oculaires a bien été récupéré."
+        res.status(200).json({message, data})
+    })
+    .catch(error => {
+        res.status(500).json({error})
+    }) 
+};
+
+exports.findAllMonture = (req, res, next) => {
+    
+    Product.hasMany(ProductAttribute, {foreignKey: 'productId'});
+    ProductAttribute.belongsTo(Product);
+    Category.hasMany(Product, {foreignKey: 'categoryId'})
+    Product.belongsTo(Category);
+    Brand.hasMany(ProductAttribute, {foreignKey: 'brandId'})
+    ProductAttribute.belongsTo(Brand);
+    MountType.hasMany(ProductAttribute, {foreignKey: 'mountTypeId'})
+    ProductAttribute.belongsTo(MountType);
+    
+
+    Product.findAll({
+        include : [
+            {
+                model: ProductAttribute,
+                include: {
+                    model: Brand
+                }
+            }, 
+            {
+                model: ProductAttribute,
+                include: {
+                    model: MountType
+                }
+            }, 
+            {
+                model: Category,
+                where : {
+                    name: "monture"
+                }
+            } 
+        ]        
+    })
+    .then(data => {
+        const message = "La liste des montures a bien été récupéré."
+        res.status(200).json({message, data})
+    })
+    .catch(error => {
+        res.status(500).json({error})
+    })
+};
+
+exports.findOneTelescope = (req, res, next) => {
+    Product.hasMany(ProductAttribute, {foreignKey: 'productId'});
+    ProductAttribute.belongsTo(Product);
+    Category.hasMany(Product, {foreignKey: 'categoryId'})
+    Product.belongsTo(Category);
+    Brand.hasMany(ProductAttribute, {foreignKey: 'brandId'})
+    ProductAttribute.belongsTo(Brand);
+    TelescopeType.hasMany(ProductAttribute, {foreignKey: 'telescopeTypeId'})
+    ProductAttribute.belongsTo(TelescopeType);
+
+    Product.findByPk(req.params.id, {
+        include : [
+            {
+                model: ProductAttribute,
+                include: {
+                    model: Brand,
+                }
+            }, 
+            {
+                model: ProductAttribute,
+                include: {
+                    model: TelescopeType,
+                }
+            }, 
+            {
+                model: Category,
+                where : {
+                    name: "telescope"
+                }
+            } 
+        ]        
+    })
+    .then(telescope => {
+        const message = "Un telescope a bien été trouvé."
+        res.status(200).json({message, data: telescope})
+    })
+    .catch(error => {
+        res.status(500).json({error})
+    })
+};
+
+exports.findOneOculaire = (req, res, next) => {
+
+    Product.hasMany(ProductAttribute, {foreignKey: 'productId'});
+    ProductAttribute.belongsTo(Product);
+    Category.hasMany(Product, {foreignKey: 'categoryId'})
+    Product.belongsTo(Category);
+    Brand.hasMany(ProductAttribute, {foreignKey: 'brandId'})
+    ProductAttribute.belongsTo(Brand);
+    OculaireCollection.hasMany(ProductAttribute, {foreignKey: 'oculaireModelId'})
+    ProductAttribute.belongsTo(OculaireCollection);
+    
+
+    Product.findByPk(req.params.id, {
+        include : [
+            {
+                model: ProductAttribute,
+                include: {
+                    model: Brand
+                }
+            }, 
+            {
+                model: ProductAttribute,
+                include: {
+                    model: OculaireCollection
+                }
+            }, 
+            {
+                model: Category,
+                where : {
+                    name: "oculaire"
+                }
+            } 
+        ]        
+    })
+    .then(telescope => {
+        const message = "Un oculaire a bien été trouvé."
+        res.status(200).json({message, data: telescope})
+    })
+    .catch(error => {
+        res.status(500).json({error})
+    })
+};
+
+exports.findOneMonture = (req, res, next) => {
+   
+    Product.hasMany(ProductAttribute, {foreignKey: 'productId'});
+    ProductAttribute.belongsTo(Product);
+    Category.hasMany(Product, {foreignKey: 'categoryId'})
+    Product.belongsTo(Category);
+    Brand.hasMany(ProductAttribute, {foreignKey: 'brandId'})
+    ProductAttribute.belongsTo(Brand);
+    MountType.hasMany(ProductAttribute, {foreignKey: 'mountTypeId'})
+    ProductAttribute.belongsTo(MountType);
+    
+
+    Product.findByPk(req.params.id, {
+        include : [
+            {
+                model: ProductAttribute,
+                include: {
+                    model: Brand
+                }
+            }, 
+            {
+                model: ProductAttribute,
+                include: {
+                    model: MountType
+                }
+            }, 
+            {
+                model: Category,
+                where : {
+                    name: "monture"
+                }
+            } 
+        ]        
+    })
+    .then(telescope => {
+        const message = "Une monture a bien été trouvé."
+        res.status(200).json({message, data: telescope})
+    })
+    .catch(error => {
+        res.status(500).json({error})
+    })
+};
--- back/routes/products.js
+++ back/routes/products.js
@@ -3,5 +3,10 @@ const router = express.Router();
 const productsController = require('../controllers/products');
 
 router.get('/telescopes', productsController.findAllTelescope)
+router.get('/telescopes/:id', productsController.findOneTelescope)
+router.get('/oculaires', productsController.findAllOculaire)
+router.get('/oculaires/:id', productsController.findOneOculaire)
+router.get('/montures', productsController.findAllMonture)
+router.get('/montures/:id', productsController.findOneMonture)
 
 module.exports = router;
