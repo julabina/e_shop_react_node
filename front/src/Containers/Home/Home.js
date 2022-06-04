ee731f28169daa1289fff597358e87381ff21f34
Lenfumé Julien <julabina@hotmail.fr>
2022-06-04 16:48:05 +0200

    first version of home page

--- front/src/Containers/Home/Home.js
+++ front/src/Containers/Home/Home.js
@@ -1,12 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { decodeToken, isExpired } from 'react-jwt';
+import telescopeImg from '../../assets/telescope.webp'
+import oculaireImg from '../../assets/oculaire.webp'
+import montureImg from '../../assets/monture.webp'
+import promoImg from '../../assets/promo.webp'
+import TelescopeCard from '../../Components/TelescopeCard/TelescopeCard';
+import OculaireCard from '../../Components/OculaireCard/OculaireCard';
+import MontureCard from '../../Components/MontureCard/MontureCard';
+import { NavLink } from 'react-router-dom';
 
 const Home = () => {
 
     const dispatch = useDispatch();
 
     const [isLogged, setIsLogged] = useState(false);
+    const [promoProduct, setPromoProduct] = useState([])
 
     useEffect(() => {
         window.scrollTo(0, 0);
@@ -41,12 +50,42 @@ const Home = () => {
             setIsLogged(false);
         }; 
 
-        
+        getPromo();
     },[])
 
+    const getPromo = () => {
+        fetch('http://localhost:3000/api/products/promotion', {
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            method : 'POST'
+        })
+            .then(res => res.json())
+            .then(products => {
+                let newArr = [];
+
+                for(let i = 0; i < 4; i++) {
+                    if(products.data[i] !== undefined) {
+                        const item = {
+                            cat: products.data[i].Category.name,
+                            name: products.data[i].name,
+                            pictures: products.data[i].pictures,
+                            price: products.data[i].price,
+                            id: products.data[i].id,
+                            promo: products.data[i].promo,
+                            promoValue: products.data[i].promoValue,
+                            stock: products.data[i].stock
+                        }
+                        newArr.push(item)
+                    }
+                }
+                setPromoProduct(newArr);
+            })
+    }
 
     return (
-        <main>
+        <main className='home'>
             <section className="home__carrousel">
 
             </section>
@@ -54,40 +93,62 @@ const Home = () => {
                 <h2>Nos catégories</h2>
                 <div className="home__categories__cont">
                     <div className="home__categories__cont__row">
-                        <div className="home__categories__cont__row__category">
-                            <h3>Telescopes</h3>
-                            <div className="home__categories__cont__row__category__imgCont">
-                                <img src="" alt="" />
+                        <NavLink to="/telescope">
+                            <div className="home__categories__cont__row__category">
+                                <h3>Telescopes</h3>
+                                <div className="home__categories__cont__row__category__imgCont">
+                                    <img src={telescopeImg} alt="" />
+                                </div>
                             </div>
-                        </div>
-                        <div className="home__categories__cont__row__category">
-                            <h3>Oculaires</h3>
-                            <div className="home__categories__cont__row__category__imgCont">
-                                <img src="" alt="" />
+                        </NavLink>
+                        <NavLink to='/oculaire'>
+                            <div className="home__categories__cont__row__category">
+                                <h3>Oculaires</h3>
+                                <div className="home__categories__cont__row__category__imgCont">
+                                    <img src={oculaireImg} alt="" />
+                                </div>
                             </div>
-                        </div>
+                        </NavLink>
                     </div>
                     <div className="home__categories__cont__row">
-                        <div className="home__categories__cont__row__category">
-                            <h3>Montures</h3>
-                            <div className="home__categories__cont__row__category__imgCont">
-                                <img src="" alt="" />
+                        <NavLink to="/monture">
+                            <div className="home__categories__cont__row__category">
+                                <h3>Montures</h3>
+                                <div className="home__categories__cont__row__category__imgCont">
+                                    <img src={montureImg} alt="" />
+                                </div>
                             </div>
-                        </div>
-                        <div className="home__categories__cont__row__category">
-                            <h3>Promotions</h3>
-                            <div className="home__categories__cont__row__category__imgCont">
-                                <img src="" alt="" />
+                        </NavLink>
+                        <NavLink to="/promotion">
+                            <div className="home__categories__cont__row__category">
+                                <h3>Promotions</h3>
+                                <div className="home__categories__cont__row__category__imgCont">
+                                    <img src={promoImg} alt="" />
+                                </div>
                             </div>
-                        </div>
+                        </NavLink>
                     </div>
                 </div>
             </section>
             <section className="home__promo">
                 <h2>Nos promotions</h2>
-                <div className="home__promo__promoCont"></div>
-                <p>Et bien d'autre encore !</p>
-                <button>Voir toutes les promotions</button>
+                <div className="home__promo__promoCont">
+                <ul>
+                    {promoProduct.map(el => {
+                        if(el.cat === "telescope") {
+                            return <TelescopeCard id={el.id} name={el.name} price={el.price} key={el.id} image={el.pictures[0]} stock={el.stock} promo={el.promo} promoValue={el.promoValue} />
+                        } else if(el.cat === "oculaire") {
+                            return <OculaireCard id={el.id} name={el.name} price={el.price} key={el.id} image={el.pictures} stock={el.stock} promo={el.promo} promoValue={el.promoValue} />
+                        } else if(el.cat === "monture") {
+                            return <MontureCard id={el.id} name={el.name} price={el.price} key={el.id} image={el.pictures} stock={el.stock} promo={el.promo} promoValue={el.promoValue} />
+                        } 
+                    })} 
+                </ul>
+                </div>
+                <p className='home__promo__para'>Et bien d'autre encore !</p>
+                <NavLink to="/promotion">
+                    <button className='home__promo__btn'>Voir toutes les promotions</button>
+                </NavLink>
             </section>
         </main>
     );
